09cd43d5f9af790c6978125e916e43a5f2df0f02
tlatmsrud <tlatmsrud@naver.com>
2021-04-14 22:35:32 +0900

    gradle project git connection

---
+++ src/test/java/com/ssk/SpringbootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
