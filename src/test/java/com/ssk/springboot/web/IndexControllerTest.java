591d35eb502b52b6d20deb991d62d19b846502a3
tlatmsrud <tlatmsrud@naver.com>
2021-04-20 02:26:35 +0900

    20200420 - stream().map().collect() 코드 분석

--- src/test/java/com/ssk/springboot/web/IndexControllerTest.java
+++ src/test/java/com/ssk/springboot/web/IndexControllerTest.java
@@ -2,12 +2,19 @@ package com.ssk.springboot.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ssk.springboot.domain.posts.Posts;
+import com.ssk.springboot.web.dto.PostsListResponseDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,6 +28,34 @@ public class IndexControllerTest {
 		String body=this.restTemplate.getForObject("/",String.class);
 		
 		assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+	}
+	
+	@Test
+	public void test() {
+		List<Posts> list = new ArrayList<Posts>();
+		list.add(Posts.builder()
+					.title("title")
+					.content("content")
+					.author("author")
+					.build()
+		);
+		list.add(Posts.builder()
+				.title("title2")
+				.content("content2")
+				.author("author2")
+				.build()
+		);
+		
+		list.stream().
+		map(PostsListResponseDto::new)
+		.collect(Collectors.toList());
+		
+		//stream.map -> stream의 각 값을 PostsListResponseDto 객체로 변환 및 생성
+		//stream.map.collection -> stream의 각 값을 PostsListResponseDto 객체로 변환 및 생성한 후 collect 메서드를 사용해 list 형식으로 변환한다.
+		//결론 : List<Posts> 데이터를 List<PostsListResponseDto> 로 변환
+		System.out.println(list.stream());
+		System.out.println(list.stream().
+				map(PostsListResponseDto::new));
 	}
 
 }
